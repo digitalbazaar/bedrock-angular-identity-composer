d8e4746042390d162c9e98150c44ad2a68554a81
Dave Longley <dlongley@digitalbazaar.com>
2017-01-16 17:14:30 -0500

    Include `bedrock-angular-card-displayer` in demo test app.
    - Rename less file to follow convention.

--- demo/components/main.js
+++ demo/components/main.js
@@ -8,7 +8,8 @@ define([
 
 'use strict';
 
-var module = angular.module('bedrock.composer-test', []);
+var module = angular.module('bedrock.composer-test', [
+  'bedrock.credential', 'bedrock.card-displayer']);
 
 Array.prototype.slice.call(arguments, 1).forEach(function(register) {
   register(module);
@@ -21,6 +22,33 @@ module.config(function($routeProvider) {
       title: 'Test',
       template: '<br-test-composer></br-test-composer>'
     });
+}).run(function(brCredentialService) {
+  brCredentialService.registerDisplayer({
+    id: 'urn:bedrock:br-motor-vehicle-credential-displayer',
+    accept: {
+      'urn:bedrock:test:MotorVehicleLicenseCredential': {}
+    },
+    directive: 'br-motor-vehicle-credential-displayer'
+  });
+
+  // generic card types
+  var cardTypes = [
+    "urn:bedrock:test:CableSubscriptionCredential",
+    "urn:bedrock:test:LoyaltyCardCredential",
+    "urn:bedrock:test:MessageBoardSubscription",
+    "urn:bedrock:test:PassportCredential",
+    "urn:bedrock:test:ProofOfResidenceCredential",
+    "urn:bedrock:test:LegalNameCredential"
+  ];
+  cardTypes.forEach(function(cardType) {
+    var accept = {};
+    accept[cardType] = {};
+    brCredentialService.registerDisplayer({
+      id: 'urn:bedrock:card:type:' + cardType,
+      accept: accept,
+      directive: 'br-credential-card-displayer'
+    });
+  });
 });
 
 });
