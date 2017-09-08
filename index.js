87c93436a7b79b6b1d682b5834a6dcb7f90292ad
Ganesh Annan <gannan08@vt.edu>
2017-09-08 15:40:41 -0400

    Start transition to material.

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ import IdentityComposerComponent from './identity-composer-component.js';
 const module = angular.module(
   'bedrock.identity-composer', ['bedrock.credential', 'bedrock.form',
     'bedrock.lazyCompile', 'bedrock.media-query', 'bedrock.modal',
-    'ngAnimate']);
+    'ngAnimate', 'ngMaterial']);
 
 module.component('brIdentityComposer', IdentityComposerComponent);
